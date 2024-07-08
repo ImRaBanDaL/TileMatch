f8bd8fff401f1c680206aa02f72ec69c0c7abe35
ImRaBanDaL <154900375+ImRaBanDaL@users.noreply.github.com>
2024-07-09 01:02:14 +0300

    Board Updated
    Prepare tiles

--- Assets/Scripts/Runtime/Managers/Board.cs
+++ Assets/Scripts/Runtime/Managers/Board.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Project Dependency")]
+    [SerializeField] Tile tilePrefab; //Tile spawnlayabilmek için.
 
-    // Update is called once per frame
-    void Update()
+    [Header("Scene Dependency")]
+    [SerializeField] Transform tileParent; //Tile'ýn spawnlanacaðý parent'ý.
+
+    /*Tile instantiate yapýldýktan sonra tutulacaðý için bir array olþturuyoruz. Daha sonra bu listeye ulaþabilmek gerekecek. Bu yüzdne dýþarýdan okunabilir ama deðiþtirilemez olarak ayarlamamýz gerekiyor. 
+     * Bunu get ve set ile yapýyoruz.*/
+
+    public Tile[] Tiles { get; private set; }
+
+
+    void PrepareTile()
     {
-        
+        var tileCount = 5;
+        Tiles = new Tile[tileCount]; //TODO: Change with level tile amount
+
+        for (int i = 0; i < tileCount; i++)
+        {
+            Tiles[i] = Instantiate(tilePrefab, tileParent);
+        }
     }
+    
 }
