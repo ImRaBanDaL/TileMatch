c0339995ab9767734878d016e182f2cc23210a5a
ImRaBanDaL <154900375+ImRaBanDaL@users.noreply.github.com>
2024-07-09 16:23:24 +0300

    Updated TouchManager
    event and interface added

--- Assets/Scripts/Runtime/Managers/TouchManager.cs
+++ Assets/Scripts/Runtime/Managers/TouchManager.cs
@@ -1,18 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngineInternal;
+
+
+public static class TouchEvents
+{
+    public static Action <ITouchable>OnElementTapped;
+    public static Action OnEmptyTapped;
+}
+
+public interface ITouchable
+{
+
+}
 
 public class TouchManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Camera cam;
+    [SerializeField] string collisionTag;
+    bool _canTouch;
+
+    private void Start()
     {
-        
+        _canTouch = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    
+
+
+    private void Update()
     {
-        
+        if (_canTouch)
+        {
+            GetTouch(Input.mousePosition);
+        }
     }
+
+    private void GetTouch(Vector3 pos)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var hit = Physics2D.OverlapPoint(cam.ScreenToWorldPoint(pos));
+
+            if (CanTouch(hit))
+            {
+                //var selectedCard = hit.gameObject.GetComponent<Card>();
+                
+            }
+        }
+    }
+
+    bool CanTouch(Collider2D hit)
+    {
+        return hit != null && hit.CompareTag(collisionTag);
+    }
+
+    IEnumerator WaitForTouch_Cor()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        _canTouch = true;
+    }
+
 }
