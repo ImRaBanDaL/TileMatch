bcbd877a260b409af3e55ea3ec27afbcb99ad158
ImRaBanDaL <154900375+ImRaBanDaL@users.noreply.github.com>
2024-07-12 00:10:11 +0300

    Tile data created

---
+++ Assets/Scripts/Runtime/Gameplay/Levels/LevelData.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public struct LevelData
+{
+    public string title;
+    public Tile[] tile;
+}
---
+++ Assets/Scripts/Runtime/Gameplay/Tiles/TileData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct TileData
+{
+    public int id;
+    public Vector3 position;
+    public string character;
+    public int[] children;
+}
