3080693ab1edb54ac7f8acf7d1cf20b8d7dd03a0
ImRaBanDaL <154900375+ImRaBanDaL@users.noreply.github.com>
2024-07-12 13:56:19 +0300

    DataHandler created

---
+++ Assets/Scripts/Runtime/Util/DataHandler.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class DataHandler
+{
+    public static void Save<T>(T saveData, string key)
+    {
+        var data = JsonUtility.ToJson(saveData);
+        PlayerPrefs.SetString(key, data);
+    }
+
+    public static T Load<T>(string key) where T : new()
+    {
+        var data = PlayerPrefs.GetString(key);
+        var loadedData = JsonUtility.FromJson<T>(data) ?? new T();
+        return loadedData;
+    }
+
+    public static bool HasData(string key)
+    {
+        return PlayerPrefs.HasKey(key);
+    }
+
+    public static void Delete(string key)
+    {
+        if (HasData(key))
+        {
+            PlayerPrefs.DeleteKey(key);
+        }
+    }
+}
