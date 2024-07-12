6aed444a279d6a2e0239015323f9900e60893b94
ImRaBanDaL <154900375+ImRaBanDaL@users.noreply.github.com>
2024-07-12 14:56:38 +0300

    LevelEvents created

---
+++ Assets/Scripts/Runtime/Gameplay/Levels/CompleteData.cs
@@ -0,0 +1,13 @@
+
+
+public struct CompleteData
+{
+    public int Score;
+    public int Index;
+
+    public CompleteData(int index, int score)
+    {
+        Index = index;
+        Score = score;
+    }
+}
